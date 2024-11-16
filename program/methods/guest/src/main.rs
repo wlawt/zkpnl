8fe46d495068a892edc6f45eb2c34895b5251454
William Law <williamlaw.wtl@gmail.com>
2024-11-16 05:00:02 -0500

    r0 proof gen (#8)
    * works
    
    * w2

--- program/methods/guest/src/main.rs
+++ program/methods/guest/src/main.rs
@@ -1,13 +1,22 @@
 use risc0_zkvm::guest::env;
 
 fn main() {
-    // TODO: Implement your guest code here
+    //get vals read in
+    let inputs: Vec<f32> = env::read();
 
-    // read the input
-    let input: u32 = env::read();
+    let entry: f32 = inputs[0];
+    let current: f32 = inputs[1];
+    let pnl_provided: f32 = inputs[2];
 
-    // TODO: do something with the input
+    //pnl formula
+    let pnl_calculated = ((current - entry) / entry) * 100.0;
 
-    // write public output to the journal
-    env::commit(&input);
+    // check if pnl matches
+    if (pnl_provided - pnl_calculated).abs() > 0.0001 {
+        panic!("PNL verification failed: provided = {}, calculated = {}", pnl_provided, pnl_calculated);
+    }
+
+    // Commit to journal
+    let success = true;
+    env::commit(&success);
 }
